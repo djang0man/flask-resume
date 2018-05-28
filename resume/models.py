70a43debc4c950b89bb135e117a0c6864d43a2d2
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-05-28 16:16:38 -0700

    Comment data.

--- resume/models.py
+++ resume/models.py
@@ -150,7 +150,7 @@ class Project(Base):
     
     profile_id = Column(Integer, ForeignKey('profile.id'))
 
-
+'''
 
 Base.metadata.create_all(engine)
 
@@ -331,4 +331,4 @@ session.add(awards[5])
 
 session.commit()
 
-
+'''
