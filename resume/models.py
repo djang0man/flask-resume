380538b2e1b9d929e6d7fe209d47452e9b15072d
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-10-06 09:39:56 -0700

    Updated

--- resume/models.py
+++ resume/models.py
@@ -197,7 +197,10 @@ institutions = [
 
 positions = [
     Position(title="Software Engineer", start_date="Jan 2018", is_current="true", company_id="1", 
-        description=""),
+        description="<ul>"\
+            "<li>Implements new features and enhancements to core product services, interfaces, and APIs.</li>"\
+            "<li>Leverages leading-edge web APIs and protocols including Service Workers, Push, Notifications, and WebSockets.</li>"\
+          "</ul>"),
     Position(title="Engineering Team Lead", start_date="Apr 2012", end_date="Jul 2017", company_id="2", 
         description="<ul>"\
             "<li>Led a team of developers in delivering responsive web applications to top hoteliers.</li>"\
